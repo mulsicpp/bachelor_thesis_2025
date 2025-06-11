73a3ed13615c575176592419d18325de71191bc4
mulsicpp <christianmulser000@gmail.com>
2025-06-11 21:58:12 +0200

    created window

--- app/app/src/main.cpp
+++ app/app/src/main.cpp
@@ -1,7 +1,42 @@
 #include <stdio.h>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 int main(void) {
-	printf("Hello world!\n");
+
+	if (!glfwInit())
+	{
+		printf("Failed to initialize GLFW\n");
+		return -1;
+	}
+
+
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Raytracing App", NULL, NULL);
+	if (!window)
+	{
+		printf("Failed to create window\n");
+
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 
 	return 0;
 }
