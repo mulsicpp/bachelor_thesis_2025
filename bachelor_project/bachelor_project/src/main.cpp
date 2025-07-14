ff37a2e18a23c420755bbbe74cd96a253170f4d2
mulsicpp <christianmulser000@gmail.com>
2025-07-15 01:10:08 +0200

    added move semantics

--- bachelor_project/bachelor_project/src/main.cpp
+++ bachelor_project/bachelor_project/src/main.cpp
@@ -2,15 +2,50 @@
 #include <stdexcept>
 
 #include "utils/LibManager.h"
+#include "utils/move.h"
+
 #include "App.h"
+
+class Test {
+public:
+	int* val;
+
+	Test() : val{ nullptr } {}
+
+	Test(int i) : val{ new int(i) } {
+		dbg_log("created %i", *val);
+	}
+
+	void destroy() {
+		dbg_log("destroyed %i", *val);
+		delete val;
+	}
+
+	void mark_moved() {
+		dbg_log("moved %i", *val);
+		val = nullptr;
+	}
+
+	bool was_moved() {
+		return val == nullptr;
+	}
+
+	MOVE_SEMANTICS(Test)
+};
 
 int main(void) {
 	try {
 
 		utils::LibManager lib_manager;
 
-		App app{};
-		app.run();
+		// App app{};
+		// app.run();
+
+		Test t1{ 3 };
+
+		Test t2{ 5 };
+
+		std::swap(t1, t2);
 	}
 	catch (const std::exception& e) {
 		fprintf(stderr, "EXCEPTION: %s\n", e.what());
---
+++ bachelor_project/bachelor_project/src/utils/move.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "dbg_log.h"
+
+#define MOVE_SEMANTICS(class_name)                     					\
+public:                                                					\
+	inline class_name(class_name&& other) noexcept {   					\
+		*this = other;                                 					\
+		other.mark_moved();                            					\
+	}                                                 					\
+																		\
+	inline class_name& operator=(class_name&& other) noexcept {			\
+		if (this == &other) return *this;								\
+																		\
+		if (!was_moved()) {												\
+			destroy();													\
+		}																\
+		*this = other;													\
+		other.mark_moved();												\
+																		\
+		return *this;													\
+	}																	\
+																		\
+	inline ~class_name() {										        \
+		if (was_moved()) return;										\
+		destroy();														\
+	}																	\
+																		\
+private:																\
+	class_name(const class_name& other) = default;						\
+	class_name& operator=(const class_name& other) = default;			\
