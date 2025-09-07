72c4fd2947bb8ee5b423a7a4ee3b5a1f2afb845f
mulsicpp <christianmulser000@gmail.com>
2025-09-07 23:18:32 +0200

    added delta time to cli options

--- bachelor_project/bachelor_project/src/app/CLIOptions.cpp
+++ bachelor_project/bachelor_project/src/app/CLIOptions.cpp
@@ -6,11 +6,21 @@
 #include <cstdlib>
 
 CLIOptions::CLIOptions() : app{ APP_DESCRIPTION, APP_NAME } {
-    auto validate_positive = CLI::Validator(
+    auto validate_positive_int = CLI::Validator(
         [](const std::string& s) {
             int64_t val = std::stoll(s);
 
             if (val <= 0) return s + " is not a positive value";
+            return std::string{};
+        },
+        "POSITIVE"
+    );
+
+    auto validate_positive_float = CLI::Validator(
+        [](const std::string& s) {
+            double val = std::stod(s);
+
+            if (val <= 0.0) return s + " is not a positive value";
             return std::string{};
         },
         "POSITIVE"
@@ -34,11 +44,15 @@ CLIOptions::CLIOptions() : app{ APP_DESCRIPTION, APP_NAME } {
 
     app.add_option("-f,--frames", frame_count, "The number of frames to be generated")
         ->default_str(std::to_string(frame_count))
-        ->check(validate_positive);
+        ->check(validate_positive_int);
 
     app.add_option("-r,--resolution", resolution, "The resolution of the frames")
         ->default_str(std::to_string(std::get<0>(resolution)) + " " + std::to_string(std::get<1>(resolution)))
-        ->check(validate_positive);
+        ->check(validate_positive_int);
+
+    app.add_option("-d,--delta-time", delta_time, "The time that passes between two frames")
+        ->default_str(std::to_string(delta_time))
+        ->check(validate_positive_float);
 
     app.set_version_flag("-v,-V,--version", APP_NAME " 1.0.0");
 }
--- bachelor_project/bachelor_project/src/app/CLIOptions.h
+++ bachelor_project/bachelor_project/src/app/CLIOptions.h
@@ -13,6 +13,7 @@ public:
     std::string target_dir{ "raytracing_results" };
     uint32_t frame_count{ 200 };
     std::tuple<uint32_t, uint32_t> resolution{ std::tuple<uint32_t, uint32_t>(IMAGE_WIDTH, IMAGE_HEIGHT) };
+    float delta_time{ 0.1f };
 
 private:
     CLI::App app{};
