3834adac3d8e896f82041059432e75ca69cf0cb8
mulsicpp <christianmulser000@gmail.com>
2025-09-15 23:47:15 +0200

    removed unnecessary folder vk_ext

--- bachelor_project/src/vk_ext/ext_macros.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-//#define EXT_FUNCS()
-#define DECL_EXT_FN(func_name) PFN_##func_name func_name
-#define LD_EXT_FN_INS(func_name) func_name = (PFN_##func_name)vkGetInstanceProcAddr(instance, #func_name)
